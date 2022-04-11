d927d148fd2ef1333637f111de9b42db537dcc1c
daulet <daulet.issatayev@nu.edu.kz>
2022-04-11 10:59:50 +0600

    feat: add CTA buttons (+ add sass)

---
+++ components/CTAButton/index.tsx
@@ -0,0 +1,41 @@
+import { Text, useMantineTheme, TextProps } from '@mantine/core';
+import styles from './index.module.scss';
+import React from 'react';
+
+interface IProps extends TextProps<'div'> {
+  icon?: React.ReactNode;
+  iconPosition?: 'left' | 'right';
+}
+
+const CTAButton: React.FC<IProps> = ({
+  icon,
+  iconPosition,
+  children,
+  ...rest
+}) => {
+  const { other } = useMantineTheme();
+
+  return (
+    <div className={styles.CTAButton}>
+      {icon && iconPosition === 'left' ? icon : null}
+      <div className={styles.text}>
+        <Text
+          variant="gradient"
+          size="lg"
+          gradient={other.gradients.brand}
+          weight={700}
+          {...rest}
+        >
+          {children}
+        </Text>
+      </div>
+      {icon && iconPosition === 'right' ? icon : null}
+    </div>
+  );
+};
+
+CTAButton.defaultProps = {
+  iconPosition: 'left',
+};
+
+export default CTAButton;
