2d1caabc0601a507f6bb557da767647a99e381e9
iantanwx <ian.tan@voyage.finance>
2022-04-28 15:20:52 +0800

    add some logs, drop this commit

--- components/organisms/DepositTrancheModal/index.tsx
+++ components/organisms/DepositTrancheModal/index.tsx
@@ -1,11 +1,12 @@
-import { Divider, Text, Title, Modal } from '@components/base';
+import { Divider, Modal, Text, Title } from '@components/base';
 import AmountInput from '@components/moleculas/AmountInput';
-import { Button, Group, Input, ModalProps, ThemeIcon } from '@mantine/core';
+import { Button, Group, ModalProps } from '@mantine/core';
 import Image from 'next/image';
-import { useAccount, useContractWrite, useSigner } from 'wagmi';
+import { useAccount, useContract, useContractWrite, useSigner } from 'wagmi';
 import Voyager from 'smartcontracts/Voyager.json';
 import Tus from 'smartcontracts/Tus.json';
 import { useEffect } from 'react';
+import { ethers } from 'ethers';
 
 type IProps = ModalProps & {
   type: 'Senior' | 'Junior';
@@ -24,6 +25,38 @@ const DepositTrancheModal: React.FC<IProps> = ({ type, ...props }) => {
     },
     'deposit'
   );
+  const voyager = useContract({
+    addressOrName: Voyager.address,
+    contractInterface: Voyager.abi,
+    signerOrProvider: signer,
+  });
+
+  useEffect(() => {
+    if (accountData?.address) {
+      voyager.populateTransaction
+        .deposit(Tus.address, '1', '100000', accountData.address)
+        .then((tx: any) => {
+          console.log('unsigned tx: ', tx);
+          const { data } = tx;
+          const fnSelector = data.slice(2, 10);
+          const expectedFnSelector = ethers.utils
+            .id('deposit(address,uint8,uint256,address)')
+            .slice(2, 10);
+          console.log('actual fn selector: ', fnSelector);
+          console.log('expected fn selector: ', expectedFnSelector);
+          console.log(
+            'computed fn selector: ',
+            voyager.interface.getSighash(
+              voyager.interface.getFunction('deposit')
+            )
+          );
+          console.log(
+            'function abi: ',
+            voyager.interface.getFunction('deposit')
+          );
+        });
+    }
+  }, [accountData, voyager]);
 
   const onDeposit = async () => {
     const data = await deposit({
