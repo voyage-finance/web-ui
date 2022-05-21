b913fc1693dc958a1f85b6755159b9a25146d3d2
daulet <daulet.issatayev@nu.edu.kz>
2022-05-22 01:54:33 +0600

    fix: let SC calls when no account connected

--- components/moleculas/UnsupportedNetworkModal/index.tsx
+++ components/moleculas/UnsupportedNetworkModal/index.tsx
@@ -1,23 +1,15 @@
 import { Button, Modal, Text, Title } from '@components/base';
 import { Group } from '@mantine/core';
-import { useEffect } from 'react';
 import { getProviderConfiguration } from 'utils/env';
-import { useNetwork } from 'wagmi';
+import { useAccount, useNetwork } from 'wagmi';
 
 const UnsupportedNetworkModal: React.FC = () => {
   const { activeChain, switchNetwork } = useNetwork();
+  const { data: accountData } = useAccount();
   const currentConf = getProviderConfiguration();
-  useEffect(() => {
-    console.log(
-      '[UnsupportedNetworkModal]',
-      activeChain,
-      switchNetwork,
-      currentConf
-    );
-  }, []);
   return (
     <Modal
-      opened={activeChain?.id !== currentConf.chainId}
+      opened={accountData !== null && activeChain?.id !== currentConf.chainId}
       onClose={() => {}}
       withCloseButton={false}
     >
