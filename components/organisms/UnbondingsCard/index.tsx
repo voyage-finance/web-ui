eb43954873d8060978af87f4f3dd02153b3f3e06
daulet <daulet.issatayev@nu.edu.kz>
2022-06-08 17:29:38 +0600

    feat: fetch unbonding list from gql

--- components/organisms/UnbondingsCard/index.tsx
+++ components/organisms/UnbondingsCard/index.tsx
@@ -1,46 +1,15 @@
 import { Button, Card, Divider, Text, Title } from '@components/base';
+import UnbondingRow from '@components/moleculas/UnbondingRow';
 import WalletNotConnectedContainer from '@components/moleculas/WalletConnectionFence';
-import { Center, Group } from '@mantine/core';
-import BigNumber from 'bignumber.js';
-import { TrancheType } from 'types';
-import { formatAmount } from 'utils/bn';
-
-type IUnbonding = {
-  type: TrancheType;
-  daysLeft: number;
-  amount: BigNumber;
-};
+import { Center, Group, LoadingOverlay } from '@mantine/core';
+import { useUserDataCtx } from 'hooks/context/usePoolDataCtx';
 
 const UnbondingsCard: React.FC = () => {
-  const unbondings: IUnbonding[] = [
-    {
-      type: TrancheType.Junior,
-      daysLeft: 0,
-      amount: new BigNumber(1593),
-    },
-    {
-      type: TrancheType.Senior,
-      daysLeft: 0,
-      amount: new BigNumber(243),
-    },
-    {
-      type: TrancheType.Junior,
-      daysLeft: 10,
-      amount: new BigNumber(1593),
-    },
-    {
-      type: TrancheType.Junior,
-      daysLeft: 14,
-      amount: new BigNumber(1593),
-    },
-    {
-      type: TrancheType.Senior,
-      daysLeft: 2,
-      amount: new BigNumber(1593),
-    },
-  ];
+  const [user, isLoading] = useUserDataCtx();
+  const unbondings = user?.unbondings || [];
   return (
     <Card style={{ height: '100%' }} px={24} py={20}>
+      <LoadingOverlay visible={isLoading} />
       <Group direction="column" style={{ height: '100%' }} align="stretch">
         <Group position="apart">
           <Title order={5}>Unbonding</Title>
@@ -62,52 +31,14 @@ const UnbondingsCard: React.FC = () => {
             </Center>
           ) : (
             <Group direction="column" mt={-5} align="stretch" spacing={0}>
-              {unbondings.map((unbonding, index) => (
-                <>
-                  <Group key={index} align="start" mt={15} mb={18}>
-                    <Center
-                      sx={(theme) => ({
-                        background:
-                          unbonding.type === TrancheType.Senior
-                            ? theme.colors['accent-blue'][5]
-                            : theme.colors['accent-pink'][6],
-                        borderRadius: 4,
-                        width: 25,
-                        height: 25,
-                      })}
-                    >
-                      <Text size="sm" weight="bold">
-                        {unbonding.type === TrancheType.Senior ? 'S' : 'JR'}
-                      </Text>
-                    </Center>
-                    <Group direction="column" spacing={0} mt={-6}>
-                      <Text size="lg">
-                        <strong>{formatAmount(unbonding.amount)}</strong> TUS
-                      </Text>
-                      <Text
-                        size="sm"
-                        type={
-                          unbonding.daysLeft === 0 ? 'success' : 'secondary'
-                        }
-                      >
-                        {unbonding.daysLeft === 0
-                          ? 'Ready'
-                          : `${unbonding.daysLeft}D Remaining`}
-                      </Text>
-                    </Group>
-                    <Button
-                      size="s"
-                      kind="secondary"
-                      ml="auto"
-                      disabled={unbonding.daysLeft > 0}
-                      sx={{ width: 80, fontSize: 11 }}
-                    >
-                      Claim
-                    </Button>
-                  </Group>
-                  {index < unbondings.length - 1 && <Divider />}
-                </>
-              ))}
+              {unbondings.map((unbonding, index) => {
+                return (
+                  <div key={index}>
+                    <UnbondingRow unbonding={unbonding} />
+                    {index < unbondings.length - 1 && <Divider />}
+                  </div>
+                );
+              })}
             </Group>
           )}
         </WalletNotConnectedContainer>
--- hooks/useGetUserPoolData.ts
+++ hooks/useGetUserPoolData.ts
@@ -1,6 +1,7 @@
 import { useQuery } from '@apollo/client';
 import { GET_USER_DATA } from '@graph/queries/user';
-import { UserPoolData } from 'types';
+import BigNumber from 'bignumber.js';
+import { TrancheType, UserPoolData } from 'types';
 import { shiftDecimals, Zero } from 'utils/bn';
 import { useAccount } from 'wagmi';
 import { useSupportedTokensCtx } from './context/useSupportedTokensCtx';
@@ -26,6 +27,7 @@ export const useGetUserPoolData = (tokenSmb: string) => {
 
 const resultToUserPoolData = (res: any): UserPoolData => {
   const poolData = res.user.poolData[0];
+  const unbondings = res.user.unbondings || [];
   const decimals = Number(poolData.decimals);
   return {
     juniorTrancheBalance: shiftDecimals(
@@ -36,6 +38,11 @@ const resultToUserPoolData = (res: any): UserPoolData => {
       poolData.seniorTrancheBalance,
       decimals
     ),
+    unbondings: unbondings.map((v: any) => ({
+      amount: shiftDecimals(v.amount, decimals),
+      time: new BigNumber(v.time),
+      type: v.type === 1 ? TrancheType.Senior : TrancheType.Junior,
+    })),
     // TODO: update when indexer will return this fields
     withdrawableJuniorTrancheBalance: Zero,
     withdrawableSeniorTrancheBalance: Zero,
