fcc32cd3baf2c4f1782f1abb1bdafad73d614cfd
iantanwx <ian.tan@voyage.finance>
2022-06-22 13:37:17 +0800

    fix: remove deprecated contracts

--- consts/addresses.ts
+++ consts/addresses.ts
@@ -1,12 +1,8 @@
 import { ChainID, Network } from '../utils/env';
 
 import Voyager from '../abi/Voyager.json';
-import LiquidityManager from '../abi/LiquidityManager.json';
-import VoyageProtocolDataProvider from '../abi/VoyageProtocolDataProvider.json';
 
 export enum VoyageContracts {
-  VoyageProtocolDataProvider,
-  LiquidityManager,
   Voyager,
 }
 
@@ -18,17 +14,7 @@ export const Deployments: Record<
     [VoyageContracts.Voyager]: {
       network: Network.Hardhat,
       abi: Voyager,
-      address: '0x5FC8d32690cc91D4c39d9d3abcBD16989F875707',
-    },
-    [VoyageContracts.LiquidityManager]: {
-      network: Network.Hardhat,
-      abi: LiquidityManager,
-      address: '0x3Aa5ebB10DC797CAC828524e59A333d0A371443c',
-    },
-    [VoyageContracts.VoyageProtocolDataProvider]: {
-      network: Network.Hardhat,
-      abi: VoyageProtocolDataProvider,
-      address: '0x809d550fca64d94Bd9F66E60752A544199cfAC3D',
+      address: '0x8A791620dd6260079BF849Dc5567aDC3F2FdC318',
     },
   },
   [ChainID.Staging]: {
@@ -37,32 +23,12 @@ export const Deployments: Record<
       abi: Voyager,
       address: '0x37Cd9dDf18Eab2cc734Efe9dd853CE23215E9dfE',
     },
-    [VoyageContracts.LiquidityManager]: {
-      network: Network.Voyage,
-      abi: LiquidityManager,
-      address: '0x6528fd2d0d84BB758ccc47d98B716cCb12c0dfB4',
-    },
-    [VoyageContracts.VoyageProtocolDataProvider]: {
-      network: Network.Voyage,
-      abi: VoyageProtocolDataProvider,
-      address: '0x88Ad3d5A82A2401EEDFd938E7d0f0134e3c91EBF',
-    },
   },
   [ChainID.Fuji]: {
     [VoyageContracts.Voyager]: {
       network: Network.Fuji,
       abi: Voyager,
-      address: '0x293540a1A293B3adcb2EEA7d8249cF392475e665',
-    },
-    [VoyageContracts.LiquidityManager]: {
-      network: Network.Fuji,
-      abi: LiquidityManager,
-      address: '0x13aA52A79b16021107A0031Ae2c672A561F52287',
-    },
-    [VoyageContracts.VoyageProtocolDataProvider]: {
-      network: Network.Fuji,
-      abi: VoyageProtocolDataProvider,
-      address: '0x41D4D41Ae35a5815a77B437932B722e85B6dD453',
+      address: '0xed4B20ed8f97a23F5770225aA30C322CCda060E8',
     },
   },
   // TODO: after mainnet deployment
@@ -70,16 +36,6 @@ export const Deployments: Record<
     [VoyageContracts.Voyager]: {
       network: Network.Avalanche,
       abi: Voyager,
-      address: '',
-    },
-    [VoyageContracts.LiquidityManager]: {
-      network: Network.Avalanche,
-      abi: LiquidityManager,
-      address: '',
-    },
-    [VoyageContracts.VoyageProtocolDataProvider]: {
-      network: Network.Avalanche,
-      abi: VoyageProtocolDataProvider,
       address: '',
     },
   },
