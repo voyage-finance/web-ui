d84594612b9181c7e504f5de581a95059658a01f
daulet <daulet.issatayev@nu.edu.kz>
2022-07-06 09:28:25 +0600

    feat: dynamically fetch borrow epoch

--- components/moleculas/PaymentRoadmap/index.tsx
+++ components/moleculas/PaymentRoadmap/index.tsx
@@ -1,6 +1,7 @@
 import { Text } from '@components/base';
 import { Box, Group, GroupProps } from '@mantine/core';
 import BigNumber from 'bignumber.js';
+import { useGetBorrowParams } from 'hooks/useGetBorrowParams';
 import moment from 'moment';
 import * as React from 'react';
 import { formatAmount } from 'utils/bn';
@@ -9,23 +10,32 @@ type IPaymentRoadmapProps = GroupProps & {
   amount: string;
   interest: BigNumber;
   symbol: string;
+  assetAddress?: string;
 };
 
 const PaymentRoadmap: React.FunctionComponent<IPaymentRoadmapProps> = ({
   amount,
   interest,
   symbol,
+  assetAddress,
   ...props
 }) => {
+  const [borrowParams] = useGetBorrowParams(assetAddress);
+  const epoch = borrowParams?.epoch?.toNumber() || 0;
   const pmtDates = [
-    moment().add(30, 'days'),
-    moment().add(60, 'days'),
-    moment().add(90, 'days'),
+    moment().add(epoch, 'days'),
+    moment().add(epoch * 2, 'days'),
+    moment().add(epoch * 3, 'days'),
   ];
   // [amount * (100+interestPercent)/100]/3
   const singlePmtAmount = new BigNumber(amount || '0')
     .multipliedBy(interest.plus(100))
     .dividedBy(300);
+
+  React.useEffect(() => {
+    console.log('[epoch]', borrowParams?.epoch.toNumber());
+  }, [borrowParams]);
+
   return (
     <Group direction="column" spacing={8} align="stretch" {...props}>
       <Group grow spacing={0}>
@@ -53,7 +63,7 @@ const PaymentRoadmap: React.FunctionComponent<IPaymentRoadmapProps> = ({
                 width: '100%',
               })}
             >
-              30D
+              {epoch}D
             </Box>
             <Text size="sm" mt={8}>
               <strong>{formatAmount(singlePmtAmount)}</strong> {symbol}
