c6d249517bc5cfafec74f1195774c2fd9eb2f29a
daulet <daulet.issatayev@nu.edu.kz>
2022-07-06 19:41:12 +0600

    feat: Loan amount status

--- components/organisms/YourLoansTable/DrawdownRow.tsx
+++ components/organisms/YourLoansTable/DrawdownRow.tsx
@@ -1,9 +1,10 @@
 import { CTAButton, Text, Title } from '@components/base';
 import AmountWithUSD from '@components/moleculas/AmountWithUSD';
 import LoanInfoPopover from '@components/moleculas/LoanInfoPopover';
+import LoanPmtStatus from '@components/moleculas/LoanPmtStatus';
 import { createStyles, Group } from '@mantine/core';
 import moment from 'moment';
-import { Drawdown, PaymentStatus, PaymentStatusTextMap } from 'types';
+import { Drawdown } from 'types';
 import { formatAmount } from 'utils/bn';
 
 const useStyles = createStyles(() => ({
@@ -25,21 +26,9 @@ const PaymentRow: React.FC<IProps> = ({ drawdown, borderBottom }) => {
   const remainingLoan = totalLoan.minus(
     drawdown.totalInterestPaid.plus(drawdown.totalPrincipalPaid)
   );
-  const status: PaymentStatus = PaymentStatus.UPCOMING;
+
   const nextDate = moment(drawdown.nextPaymentDue * 1000).format('DD MMM');
   const borrowDate = moment(drawdown.borrowAt * 1000).format('DD MMM');
-
-  // const statusColor = (function () {
-  //   switch (status) {
-  //     case PaymentStatus.UPCOMING:
-  //       return '#FFA620';
-  //     case PaymentStatus.LATE:
-  //     case PaymentStatus.LIQUIDATED:
-  //       return '#F41B6A';
-  //     case PaymentStatus.PAID:
-  //       return '#0CCDAA';
-  //   }
-  // })();
 
   return (
     <tr className={borderBottom ? classes.borderedRow : undefined}>
@@ -81,9 +70,7 @@ const PaymentRow: React.FC<IProps> = ({ drawdown, borderBottom }) => {
         <Text align="right">{nextDate}</Text>
       </td>
       <td>
-        <Text align="right" style={{ color: '#FFA620' }}>
-          {PaymentStatusTextMap[status]}
-        </Text>
+        <LoanPmtStatus drawdown={drawdown} />
       </td>
       <td>
         <Group position="right">
