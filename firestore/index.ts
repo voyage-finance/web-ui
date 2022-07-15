35b014d5789b6286ee1bae351dd9c5126af608ff
daulet <daulet.issatayev@nu.edu.kz>
2022-07-15 16:12:46 +0600

    feat: get firebase config from .env

--- firestore/index.ts
+++ firestore/index.ts
@@ -7,17 +7,18 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: 'AIzaSyABpOcsIRxfQgftb9MDADME2HGLlTcYwJY',
-  authDomain: 'voyage-protocol.firebaseapp.com',
-  projectId: 'voyage-protocol',
-  storageBucket: 'voyage-protocol.appspot.com',
-  messagingSenderId: '590897144911',
-  appId: '1:590897144911:web:f0d2442b29e23f9ac0f63a',
-  measurementId: 'G-82BKGD1FTE',
-};
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGAING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
 const firestore = getFirestore(app);
 export default firestore;
