7ed4b93d61468e1e76895c244b0ab812a833f81a
daulet <daulet.issatayev@nu.edu.kz>
2022-07-30 10:58:19 +0600

    feat: decode email fingerprint from url

--- utils/hash.ts
+++ utils/hash.ts
@@ -2,8 +2,9 @@ export const shortenHash = (hash: string) =>
   `${hash.substring(0, 6)}...${hash.slice(-4)}`;
 
 export const decodeEmailNFingerprint = (encoded: string) => {
-  // TODO: decode encoded into email and fingerprint
-  const decodedObj = JSON.parse(encoded);
-  console.log('decodedObj', decodedObj);
+  const whitespaceReplaced = encoded.replaceAll(' ', '+');
+  const decodedObj = JSON.parse(
+    decodeURIComponent(escape(atob(whitespaceReplaced)))
+  );
   return [decodedObj.email, [...(decodedObj.fingerprint || [])]];
 };
