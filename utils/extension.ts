97e9edd7e37a3227b406f46d7d66bad3d4cb4efb
iantanwx <ian.tan@voyage.finance>
2022-09-28 16:08:19 +0800

    fix: log extension id

--- utils/extension.ts
+++ utils/extension.ts
@@ -6,6 +6,7 @@ export const sendExtensionMessage = (
   _callback?: (response: any) => void
 ) => {
   const callback = _callback ? _callback : () => undefined;
+  console.log('extension id: ', EXTENSION_ID);
   if (chrome.runtime) chrome.runtime.sendMessage(EXTENSION_ID, msg, callback);
   else {
     alert('Extension not found');
