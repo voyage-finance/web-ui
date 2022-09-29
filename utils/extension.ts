38310826d7ed9257d83c56a5dc96d7dba3c25283
daulet <daulet.issatayev@nu.edu.kz>
2022-09-29 12:02:38 +0600

    fix: use dynamic extension id

--- utils/extension.ts
+++ utils/extension.ts
@@ -1,13 +1,13 @@
 import { RuntimeMessage } from 'types';
-import { EXTENSION_ID } from 'utils/env';
 
 export const sendExtensionMessage = (
   msg: RuntimeMessage,
+  extensionId: string,
   _callback?: (response: any) => void
 ) => {
   const callback = _callback ? _callback : () => undefined;
-  console.log('extension id: ', EXTENSION_ID);
-  if (chrome.runtime) chrome.runtime.sendMessage(EXTENSION_ID, msg, callback);
+  console.log('extension id: ', extensionId);
+  if (chrome.runtime) chrome.runtime.sendMessage(extensionId, msg, callback);
   else {
     alert('Extension not found');
   }
