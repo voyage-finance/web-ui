2f157705416d7071df6ccc8c958698651f3bbf17
iantanwx <ian.tan@voyage.finance>
2022-10-05 16:30:18 +0800

    feat: migrate to auth0

--- typings/global.d.ts
+++ typings/global.d.ts
@@ -5,6 +5,8 @@ declare global {
       NEXT_PUBLIC_CHAIN_ID?: string;
       NEXT_PUBLIC_PROVIDER_URL?: string;
       NEXT_PUBLIC_GRAPH_URL?: string;
+      NEXT_PUBLIC_AUTH0_DOMAIN: string;
+      NEXT_PUBLIC_AUTH0_CLIENT_ID: string;
     }
   }
 }
