d5672e768eb6f75f0c58501da289a0505b75da29
daulet <daulet.issatayev@nu.edu.kz>
2022-10-12 21:05:49 +0600

    feat: retry if onboard session fail

---
+++ pages/onboard/steps/WrongBrowserStep.tsx
@@ -0,0 +1,39 @@
+import { Card, Text } from '@components/base';
+import { Group } from '@mantine/core';
+import Image from 'next/image';
+import * as React from 'react';
+import SwordImg from 'assets/sword.png';
+
+const WrongBrowserStep: React.FC = () => {
+  return (
+    <Card
+      style={{
+        width: 420,
+        margin: 'auto',
+        padding: '40px 53px',
+      }}
+    >
+      <Group direction="column" align={'center'} spacing={0}>
+        <Image src={SwordImg.src} alt="Voyage logo" height={84} width={111} />
+        <Text
+          sx={{ fontSize: 24 }}
+          mt={18}
+          weight={'bold'}
+          type="gradient"
+          align="center"
+        >
+          Oops, something went wrong!
+        </Text>
+        <Text mt={16} align="center" weight={'bold'}>
+          It seems like you’ve opened the magic link on a different browser from
+          the Voyage App.
+        </Text>
+        <Text align="center" mt={14}>
+          Please access the magic link on the same browser to log in.
+        </Text>
+      </Group>
+    </Card>
+  );
+};
+
+export default WrongBrowserStep;
--- utils/extension.ts
+++ utils/extension.ts
@@ -7,8 +7,5 @@ export const sendExtensionMessage = (
 ) => {
   const callback = _callback ? _callback : () => undefined;
   console.log('extension id: ', extensionId);
-  if (chrome.runtime) chrome.runtime.sendMessage(extensionId, msg, callback);
-  else {
-    alert('Extension not found');
-  }
+  chrome.runtime.sendMessage(extensionId, msg, callback);
 };
